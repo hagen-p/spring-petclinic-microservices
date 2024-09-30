3afcd60c8b38dea3810a0051feea0c2829e19edf
Pieter Hagen <phagen@splunk.com>
2024-09-30 10:50:29 +0200

    Fixed Rum  by making the java code write the env file

--- spring-petclinic-api-gateway/build-api-gateway-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-#mvn clean install -DskipTests=true  # Run Maven build command slkipping tests
-docker buildx build --platform=linux/amd64,linux/arm64 \
-    --build-arg ARTIFACT_NAME="spring-petclinic-api-gateway-0.0.1" \
-    --build-arg DOCKERIZE_VERSION="v0.8.0" \
-    -t quay.io/phagen/spring-petclinic-api-gateway:0.1  \
-    --no-cache --push . 
